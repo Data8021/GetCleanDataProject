ac01aed407596ce136a3c2d7e7ec85540b3375ee
Data8021 <bwales@gmail.com>
2015-06-09 21:52:12 +0000

    Tidy data set code complete.

---
+++ run_analysis.R
@@ -0,0 +1,62 @@
+library(dplyr)
+library(reshape2)
+
+## Read in test data, subject list and activity list
+xtest <- read.table("data/test/X_test.txt", header = FALSE, sep = "")
+xtestSubjects <- read.table("data/test/subject_test.txt")
+xtestActivity <- read.table("data/test/y_test.txt")
+
+## Add subjects and activity to test dataset
+xtest <- cbind(xtest, xtestSubjects, xtestActivity)
+
+## Read in test data, subject list and activity list
+xtrain <- read.table("data/train/X_train.txt", header = FALSE, sep = "")
+xtrainSubjects <- read.table("data/train/subject_train.txt")
+xtrainActivity <- read.table("data/train/y_train.txt")
+
+## Add subjects and activity to test dataset
+xtrain <- cbind(xtrain, xtrainSubjects, xtrainActivity)
+
+## Remove non-relevant variables and dataframes
+rm(xtestActivity, xtestSubjects, xtrainActivity, xtrainSubjects)
+
+## Bind the training and testing datasets
+fullData <- rbind(xtest, xtrain)
+rm(xtest, xtrain)
+
+## Remove duplicate column names
+colnames(fullData) <- c(seq(1:562), "activitynum")
+
+## Merge in activity lebels
+activityList <- read.table("data/activity_labels.txt", sep=" ")
+colnames(activityList) <- c("activitynum", "activity")
+fullData <- left_join(fullData, activityList, by="activitynum")
+fullData <- select(fullData, -activitynum)
+rm(activityList)
+
+## Read in variable names and apply to columns
+featureList <- read.table("data/features.txt", sep = " ", stringsAsFactor = FALSE)
+featureList <- featureList[, 2]
+featureList[562] <- "subjects"
+featureList[563] <- "activity"
+colnames(fullData) <- featureList
+
+## Identify mean and standard deviation variables (except those
+## without a standard deviation), and creat new dataframe with 
+## those variables
+varMeanStd <- intersect(grep("mean|std",featureList),
+                        grep("Freq",featureList,invert=TRUE))
+tidyData <- fullData[, c(varMeanStd, 562, 563)]
+rm(varMeanStd, featureList, fullData)
+
+## Melt into tidy dataset and Split out variable column into feature,
+## function and axis.
+tidyData <- melt(tidyData, id.vars = c("subjects", "activity"))
+tidyData <- cbind(tidyData, colsplit(tidyData$variable,
+                   "-",
+                   names = c("feature", "function", "axis")))
+tidyData <- select(tidyData, -variable) %>%
+                   select(1, 2, 4, 5, 6, 3)
+
+## Save RDA
+save(fullData, file="fullData.rda")
